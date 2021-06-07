78ca1efc66a305f5ddd145d7ee198178f1c83fb4
lauri723 <Voja@DESKTOP-DCNG86L>
2021-06-07 09:34:52 -0500

    new commit

---
+++ routes/cart.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const artworks = require('../controllers/artworks');
+const catchAsync = require('../utils/catchAsync');
+// const { isLoggedIn, isAuthor, validateCollection } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
+const Artwork = require('../models/artwork');
+
+router.get('/view', function (req, res, next) {
+    const artworks = req.session.cart;
+    res.render('cart', {
+        artworks: artworks,
+        cart: req.session.cart
+        // title: "Shopping Cart"
+    });
+});
+
+router.get('/checkout', function (req, res, next) {
+    const items = req.session.cart
+    res.render('checkout', {
+        items,
+        stripePublicKey: process.env.STRIPE_PUBLIC_KEY
+    });
+});
+
+
+router.get('/add/:artwork', catchAsync(async function (req, res) {
+    const slug = req.params.artwork
+    try {
+        let artwork = await Artwork.findOne({ slug: slug });
+        if (req.session.cart === undefined) {
+            req.session.cart = [];
+            req.session.cart.push({
+                id: artwork._id,
+                title: artwork.title,
+                qty: 1,
+                price: artwork.price,
+                photos: artwork.photos    //(req.files.map(f => ({ url: f.path, filename: f.filename })))
+            });    
+        } else {
+            let item = req.session.cart.find(item => {
+                return item.id == artwork._id;
+            });
+    
+            if (item) {
+                item.qty++;
+            } else {
+                req.session.cart.push({
+                    id: artwork._id,
+                    title: artwork.title,
+                    qty: 1,
+                    price: artwork.price,
+                    photos: artwork.photos  //(req.files.map(f => ({ url: f.path, filename: f.filename })))
+                });
+            }
+        }
+        res.redirect('/cart/view')
+    } catch(err) {
+        console.log(err);
+    }
+}));
+
+router.get('/update/:artwork', function (req, res, next) {
+    let action = req.query.action;
+    let artwork = req.session.cart.find(item => {
+        return item.id == req.params.artwork;
+    });
+    let index = req.session.cart.indexOf(artwork);
+    switch (action) {
+        case 'add':
+            artwork.qty++;
+            break;
+        case 'min':
+            artwork.qty--;
+            if (artwork.qty < 1) {
+                req.session.cart.splice(index, 1)
+            }
+            break;
+        case "clear":
+            req.session.cart.splice(index, 1);
+            if (artwork.qty <= 0)
+                delete req.session.cart;
+            break;
+        
+        default:
+            res.redirect('/cart/view');
+            break;
+    }
+    res.redirect('/cart/view')
+    // res.redirect('/cart/view', {
+    //     title: "Shopping Cart"
+    // });
+});
+
+router.get('/cancel', function(req, res, next) {
+    res.redirect('/artworks');
+});
+
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const Collection = require('../models/collection')
+const Artwork = require('../models/artwork')
+const Leaf = require('../models/leaf')
+const Student = require('../models/student')
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+router.get('/', async (req, res) => {
+    const collections = await Collection.find({}).sort({ orderKey: 1 }).sort({ createdAt: 'desc' })
+    const leaves = await Leaf.find({}).sort({ orderKey: 1 })
+    res.render('index', {
+        collections,
+        leaves,
+        searchOptions: req.query,
+    })
+})
+
+router.get('/admin', async (req, res) => {
+    const collection = await Collection.findOne({ slug: req.params.slug })
+    const collections = await Collection.find({}).sort({ createdAt: 'desc' })
+    const artworks = await Artwork.find({})
+    const artwork = await Artwork.findOne({ slug: req.params.slug })
+    const leaves = await Leaf.find({}).sort({ createdAt: 'desc' })
+    const leaf = await Leaf.findOne({ slug: req.params.slug })
+    const students = await Student.find({}).sort({ createdAt: 'desc' })
+    const student = await Student.findOne({ slug: req.params.slug })
+    res.render('admin', { collections, collection, artwork, artworks, leaves, leaf, student, students })
+})
+
+router.post("/create-payment-intent", async (req, res) => {
+    let { items } = req.body
+    items = items.map(item => item.id)
+    let total = 0
+    let amount;
+    try {
+        let docs = await Artwork.find({
+            '_id': { $in: items}
+        })
+        docs.forEach(doc => total += doc.price)
+        amount = total * 100
+    } catch(err) {
+        console.log(err)
+    }
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "usd"
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+        total
+    });
+});
+
+module.exports = router
